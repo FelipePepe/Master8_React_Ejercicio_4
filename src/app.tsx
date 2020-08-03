383ddbef9fbf0c33e97314699c8919c83a70ea8b
Felipe Pepe <FelipePepe@gmail.com>
2020-08-03 13:37:20 +0200

    Clean code and add mock datas

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,6 @@
 import React from "react";
 import { PrincipalContainer } from "./components";
 
-import { Image } from "./components/imageList/api/image-list.api.model";
-
-interface Props {
-  ImageCollection: Image[];
-}
-
 export const App: React.FC = () => {
   return <PrincipalContainer />;
 };
