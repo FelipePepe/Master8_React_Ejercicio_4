2f1ed0572b69e8e0bce968bbf447c1cbe7c3bbb6
Felipe Pepe <FelipePepe@gmail.com>
2020-08-05 15:04:53 +0200

    replace buttons with tabs of material-ui

--- src/components/principal/principal.container.tsx
+++ src/components/principal/principal.container.tsx
@@ -1,21 +1,51 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
 import { PrincipalComponent } from "./principal.component";
 import { Image } from "../imagelist/image-list.vm";
 import { getCatsCollection, getKitiesCollection } from "../imageList/api";
 import { mapImageListFromApiToVM } from "../imageList/image-list.mapper";
-import { useParams, useHistory } from "react-router-dom";
-import { routes } from "../router/router";
-import { IconButton } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 export const PrincipalContainer: React.FC = () => {
   const [images, setImages] = React.useState<Image[]>([]);
-  const server = useParams();
-  const history = useHistory();
+  const [value, setValue] = React.useState(0);
 
-  const onLoadImageList = async () => {
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const TabPanel = (props) => {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={3}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  };
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+  const onLoadImageList = async (server: string) => {
     const apiImageList =
-      server && server.id == "kities"
+      server == "kities"
         ? await getKitiesCollection()
         : await getCatsCollection();
     const viewModelImageList = mapImageListFromApiToVM(apiImageList);
@@ -23,30 +53,35 @@ export const PrincipalContainer: React.FC = () => {
   };
 
   React.useEffect(() => {
-    onLoadImageList();
+    onLoadImageList("cats");
   }, []);
 
-  const handleChangeImages = (id: string) => {
-    history.push(routes.cats(id));
-  };
+  React.useEffect(() => {
+    if (value === 0) {
+      onLoadImageList("cats");
+    } else {
+      onLoadImageList("kities");
+    }
+  }, [value]);
 
   return (
     <div>
-      <IconButton
-        onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-          handleChangeImages("cats")
-        }
-      >
-        Cats
-      </IconButton>
-      <IconButton
-        onClick={(e: React.MouseEvent<HTMLButtonElement>) =>
-          handleChangeImages("kities")
-        }
-      >
-        Kities
-      </IconButton>
-      <PrincipalComponent ImageCollection={images} />
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab label="Cats" />
+          <Tab label="Kities" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <PrincipalComponent ImageCollection={images} />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <PrincipalComponent ImageCollection={images} />
+      </TabPanel>
     </div>
   );
 };
